f2c707c56e5313379e63361a410c9c054e37b1fc
ov1n <39676588+ov1n@users.noreply.github.com>
2019-12-19 23:38:40 +0530

    Deleted cpp for Binary tree and added C
    Binary tree added in C, need to fix deletion

--- test.cpp
+++ test.cpp
@@ -1,33 +1,57 @@
 #include<iostream>
 using namespace std;
 
-
-class test{
+class bin_tree{
+	
 	private:
-		struct a{
-			int num;
-			int *g;
+		struct node{
+			int value;
+			struct node* right;
+			struct node* left;
 		};
-		struct a *point;
+		struct node *root;
 		
 	public:
-		test();/*{
-			point=new a;
-			point->num=43;
-		}*/
+		bin_tree();
+		bin_tree(int data);
+		~bin_tree();
+		struct node* createNode();
+		void insert(int data);
+		void search(struct node *ptr); //change pointer, only temp
+		//void delete(int data);
+		int display(); 		// test function for dev only
 		
-		void display(){
-			cout<<"The data is "<<(point->num)<<endl;
-		} 
 };
+
 int main(){
-	
-	test user=test();
-	user.display();
-	
+	bin_tree bree=bin_tree(44);
+	cout<<bree.display();
+	return 0;
 }
 
-test::test(){
-			point=new a;
-			point->num=43;
-		}
+bin_tree::bin_tree(int data){
+	root=new node;
+	root->value=data;
+	root->left=NULL;
+	root->right=NULL;
+}
+
+void bin_tree::search(struct node *ptr){
+	 	
+	if(ptr==NULL){
+		return;
+	}
+	search(ptr->left);
+	search(ptr->right);
+	delete[]ptr;
+}
+
+bin_tree::~bin_tree(){
+	search(root); 		//local pointer to delete tree by traversing from one node to another till end
+}
+
+bin_tree::display(){
+	return root->value;
+}
+
+
